5645cd08db4138d9c988611b03e2e970d9b0021b
kyori19 <kyori.dis@gmail.com>
2018-11-22 20:21:50 +0900

    null値のエラーを修正

--- db/migrate/20181122090024_add_cat_to_accounts.rb
+++ db/migrate/20181122090024_add_cat_to_accounts.rb
@@ -1,7 +1,8 @@
 class AddCatToAccounts < ActiveRecord::Migration[5.2]
   def up
-    add_column :accounts, :cat, :boolean, null: false
+    add_column :accounts, :cat, :boolean
     change_column_default :accounts, :cat, false
+    change_column_null :accounts, :cat, false, false
   end
 
   def down
